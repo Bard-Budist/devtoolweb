c52ae71508935bfd770d7cfc7f3fb246b2aef533
Bard Budist <op7puro@live.com>
2023-03-20 18:36:37 -0500

    Version 0.1.3

--- src/config-env.ts
+++ src/config-env.ts
@@ -1,2 +1,2 @@
-// export const urlBack = 'https://api.selecu.net/api-selecu';
-export const urlBack = 'http://localhost:3001';
+export const urlBack = 'https://api.selecu.net/api-selecu';
+// export const urlBack = 'http://localhost:3001';
